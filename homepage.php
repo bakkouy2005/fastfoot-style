e651432ba105e221012897f40494aa5b0d408c73
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 15:53:22 +0200

    Auto-commit: 2025-06-18 15:53:22

--- homepage.php
+++ homepage.php
@@ -3,9 +3,11 @@
 get_header();
 ?>
 
+<?php get_template_part('template-parts/hero-part'); ?>
+
 <div class="bg-[#181d18]">
     <div class="container mx-auto px-4">
-        <?php get_template_part('template-parts/hero-part'); ?>
+        <!-- Content after hero section goes here -->
     </div>
 </div>
 
