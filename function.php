e118be7c6888f75f80a90f6936cd0ee5034da4d9
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 10:42:19 +0200

    Auto-commit: 2025-06-18 10:42:19

--- function.php
+++ function.php
@@ -6,4 +6,9 @@ function fastfoot_style_setup() {
     add_theme_support('post-thumbnails'); 
     add_theme_support('block-templates'); 
 }
-add_action('after_setup_theme', 'fastfoot_style_setup');edd
+add_action('after_setup_theme', 'fastfoot_style_setup');
+
+function fastfoot_style_enqueue_scripts() {
+    wp_enqueue_style('fastfoot-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'fastfoot_style_enqueue_scripts');
