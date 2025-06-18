77531e67bf23ea79d4a1aeea90f90d04bd293748
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 11:15:15 +0200

    Auto-commit: 2025-06-18 11:15:15

--- header.php
+++ header.php
@@ -1,64 +1,46 @@
-!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Figma Export</title>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<div style="width: 1280px; padding-left: 40px; padding-right: 40px; padding-top: 12px; padding-bottom: 12px; border-bottom: 1px #E5E8EB solid; justify-content: flex-start; align-items: center; display: inline-flex">
-  <div style="width: 11.02px; height: 5.84px; background: white"></div>
-  <div style="width: 54.68px; height: 43px; background: #EFEAEA"></div>
-  <img style="width: 109px; height: 65px" src="https://placehold.co/109x65" />
-  <div style="flex: 1 1 0; justify-content: flex-end; align-items: flex-start; gap: 32px; display: flex">
-    <div style="width: 348px; height: 40px; justify-content: flex-start; align-items: center; gap: 36px; display: flex">
-      <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-        <div style="color: white; font-size: 14px; font-family: Be Vietnam Pro; font-weight: 500; line-height: 21px; word-wrap: break-word">Men</div>
-      </div>
-      <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-        <div style="color: white; font-size: 14px; font-family: Be Vietnam Pro; font-weight: 500; line-height: 21px; word-wrap: break-word">Women</div>
-      </div>
-      <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-        <div style="color: white; font-size: 14px; font-family: Be Vietnam Pro; font-weight: 500; line-height: 21px; word-wrap: break-word">Kids</div>
-      </div>
-      <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-        <div style="color: white; font-size: 14px; font-family: Be Vietnam Pro; font-weight: 500; line-height: 21px; word-wrap: break-word">Sale</div>
-      </div>
-      <div style="flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-        <div style="color: white; font-size: 14px; font-family: Be Vietnam Pro; font-weight: 500; line-height: 21px; word-wrap: break-word">Contact</div>
-      </div>
-    </div>
-    <div style="justify-content: flex-start; align-items: flex-start; gap: 8px; display: flex">
-      <div style="height: 40px; max-width: 480px; padding-left: 10px; padding-right: 10px; background: #293829; overflow: hidden; border-radius: 12px; justify-content: center; align-items: center; gap: 8px; display: flex">
-        <div style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
-          <div style="align-self: stretch; flex: 1 1 0; position: relative; overflow: hidden">
-            <div style="width: 20px; height: 20px; left: 0px; top: 0px; position: absolute; background: white"></div>
-            <div style="left: undefined; top: undefined; position: absolute"></div>
-          </div>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="depth-frame">
+      <div class="group">
+        <div class="overlap-group">
+          <img class="vector" src="img/image.svg" />
+          <img class="img" src="img/vector.svg" />
+          <img class="bildschirmfoto" src="img/bildschirmfoto-2025-06-17-um-23-10-42-removebg-preview-1.png" />
         </div>
       </div>
-      <div style="height: 40px; max-width: 480px; padding-left: 10px; padding-right: 10px; background: #293829; overflow: hidden; border-radius: 12px; justify-content: center; align-items: center; gap: 8px; display: flex">
-        <div style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
-          <div style="align-self: stretch; flex: 1 1 0; position: relative; overflow: hidden">
-            <div style="width: 20px; height: 20px; left: 0px; top: 0px; position: absolute; background: white"></div>
-            <div style="left: undefined; top: undefined; position: absolute"></div>
-          </div>
+      <div class="div">
+        <div class="div-2">
+          <div class="div-wrapper"><div class="text-wrapper">Men</div></div>
+          <div class="div-wrapper"><div class="text-wrapper">Women</div></div>
+          <div class="div-wrapper"><div class="text-wrapper">Kids</div></div>
+          <div class="div-wrapper"><div class="text-wrapper">Sale</div></div>
+          <div class="div-wrapper"><div class="text-wrapper">Contact</div></div>
         </div>
-      </div>
-      <div style="height: 40px; max-width: 480px; padding-left: 10px; padding-right: 10px; background: #293829; overflow: hidden; border-radius: 12px; justify-content: center; align-items: center; gap: 8px; display: flex">
-        <div style="flex: 1 1 0; flex-direction: column; justify-content: flex-start; align-items: center; display: inline-flex">
-          <div style="align-self: stretch; flex: 1 1 0; position: relative; overflow: hidden">
-            <div style="width: 20px; height: 20px; left: 0px; top: 0px; position: absolute; background: white"></div>
-            <div style="left: undefined; top: undefined; position: absolute"></div>
+        <div class="div-3">
+          <div class="depth-frame-wrapper">
+            <div class="depth-frame-wrapper-2">
+              <div class="vector-wrapper"><img class="vector-2" src="img/vector-0.svg" /></div>
+            </div>
+          </div>
+          <div class="depth-frame-wrapper">
+            <div class="depth-frame-wrapper-2">
+              <div class="vector-wrapper"><img class="vector-2" src="img/vector-0-3.svg" /></div>
+            </div>
+          </div>
+          <div class="depth-frame-wrapper">
+            <div class="depth-frame-wrapper-2">
+              <div class="vector-wrapper"><img class="vector-3" src="img/vector-0-2.svg" /></div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-</body>
+  </body>
 </html>
