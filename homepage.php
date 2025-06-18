890b1102c6b4b3e95f8bdf7cfa6d707f169155df
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 14:23:01 +0200

    Auto-commit: 2025-06-18 14:23:01

--- homepage.php
+++ homepage.php
@@ -5,10 +5,23 @@ get_header();
 
 ?>
 
-<div class="">
-    
-   
-</div>
+<section class="hero" style="background-image: url('<?php the_field('hero_background_image'); ?>')">
+  <div class="overlay">
+    <div class="hero-content">
+      <h1><?php the_field('hero_heading'); ?></h1>
+      <p><?php the_field('hero_subtext'); ?></p>
+
+      <form class="hero-search-form" action="/search" method="get">
+        <input 
+          type="text" 
+          name="s" 
+          placeholder="<?php the_field('hero_search_placeholder'); ?>">
+        <button type="submit"><?php the_field('hero_search_button_text'); ?></button>
+      </form>
+    </div>
+  </div>
+</section>
+
 
 <?php
 
