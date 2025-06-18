d3d15426bab761c0f9eac8a5fd277802a7caebd3
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 12:20:21 +0200

    Auto-commit: 2025-06-18 12:20:21

--- function.php
+++ function.php
@@ -4,27 +4,7 @@
  * 
  */
 
-// Theme setup function
-function fastfoot_style_setup() {
-    add_theme_support('title-tag'); 
-    add_theme_support('post-thumbnails'); 
-    add_theme_support('block-templates'); 
-   
-    
-    // Register navigation menus
-    register_nav_menus(
-        array(
-            'primary-menu' => esc_html__('Primary Menu', 'fastfoot-style'),
-        )
-    );
-}
-add_action('after_setup_theme', 'fastfoot_style_setup');
 
-// Enqueue scripts and styles
-function registreer_mijn_menu() {
-    register_nav_menu('menu', __('menu'));
-}
-add_action('after_setup_theme', 'registreer_mijn_menu');
 
 // Add custom classes to menu items
 
