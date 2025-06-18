fe9a8a16e3fbe960237c4ec093b32ba842ee09cd
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 22:53:47 +0200

    Auto-commit: 2025-06-18 22:53:47

--- template-parts/product.php
+++ template-parts/product.php
@@ -1,12 +1,7 @@
-<div>
-    <a href=""1</a>
-    <a href=""2</a>
-    <a href=""3</a>
-    <a href=""4</a>
-    <a href=""5</a>
-    <a href=""6</a>
-    <a href=""7</a>
-    <a href=""8</a>
-    <a href=""9</a>
-    <a href=""10</a>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="bg-white rounded-lg p-4">
+        <img src="https://via.placeholder.com/300" alt="Product 1" class="w-full h-48 object-cover rounded-lg">
+        <h3 class="text-lg font-bold mt-2">Product 1</h3>
+        <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
 </div>
