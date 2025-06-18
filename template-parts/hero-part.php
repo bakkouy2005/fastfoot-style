b1f68e40fb192ca2093a4df7dbe2d3fcf326d55f
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 15:17:52 +0200

    Auto-commit: 2025-06-18 15:17:52

--- template-parts/hero-part.php
+++ template-parts/hero-part.php
@@ -8,7 +8,7 @@ $search_group = !empty($hero_group['search_group']) ? $hero_group['search_group'
   class="relative w-full min-h-screen bg-cover bg-center rounded-lg p-8 md:p-12" 
   style="background-image: url('<?php echo !empty($hero_group['hero_background_image']) ? $hero_group['hero_background_image'] : ''; ?>');"
 >
-  <div class="absolute inset-0 bg-black/60 rounded-lg"></div> <!-- darker overlay -->
+ 
 
   <div class="relative z-10 h-full flex flex-col justify-center max-w-7xl mx-auto px-8">
     
