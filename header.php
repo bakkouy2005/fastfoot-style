4c27bdb9b04ff08bb28a23d70d241be186423ebf
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 11:33:18 +0200

    Auto-commit: 2025-06-18 11:33:18

--- header.php
+++ header.php
@@ -1,89 +1,67 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
 
-<div id="page" class="site">
-    <header id="masthead" class="site-header">
-        <div class="container">
-            <div class="site-branding">
-                <?php
-                if (is_front_page() && is_home()) :
-                    ?>
-                    <h1 class="site-title">
-                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                            <?php bloginfo('name'); ?>
-                        </a>
-                    </h1>
-                    <?php
-                else :
-                    ?>
-                    <p class="site-title">
-                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                            <?php bloginfo('name'); ?>
-                        </a>
-                    </p>
-                    <?php
-                endif;
-                ?>
-            </div>
 
-            <nav id="site-navigation" class="main-navigation">
-                <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'menu-1',
-                        'menu_id'        => 'primary-menu',
-                    )
-                );
-                ?>
+
+
+</head>
+<body>
+    <header class="bg-gradient-to-r from-black via-black to-transparent bg-opacity-90 shadow-md ">
+        <div class="container mx-auto flex justify-between items-center py-4 px-6">
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="text-2xl font-bold text-orange-600">
+                Wheely Good cars!
+            </a>
+            
+            <nav class="hidden md:flex space-x-6 text-white">
+                <a href="#" class="text-white hover:text-blue-600">alle autos</a>
+                <a href="#" class="text-white hover:text-blue-600">Verkopen</a>
+                <a href="#" class="text-white hover:text-blue-600">Financiering</a>
+                <?php if (!is_user_logged_in()) : ?>
+                    <a href="http://localhost/wordpress/login" class="text-white hover:text-blue-600">Inloggen</a>
+                <?php endif; ?>
             </nav>
+            <div class="hidden md:flex space-x-4 items-center">
+                <?php if (!is_user_logged_in()) : ?>
+                    <a href="<?php echo esc_url(home_url('/register-pagina')); ?>" class="border border-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-600 hover:text-white">
+                        Registreren
+                    </a>
+                <?php endif; ?>
+                <a href="<?php echo esc_url(home_url('/verkopen')); ?>" class="bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-orange-700">
+                    Advertentie plaatsen
+                </a>
+                <?php if (is_user_logged_in()) : ?>
+                    <div class="relative">
+                        <button id="user-menu-button" class="flex items-center space-x-2 text-white focus:outline-none">
+                            <i class="fas fa-user"></i>
+                            <span class="mx-auto"><?php echo wp_get_current_user()->display_name; ?></span>
+                            <i class="fas fa-chevron-down"></i>
+                        </button>
+                        <div id="user-menu" class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20 hidden">
+                            <a href="<?php echo esc_url(home_url('/mijn-aanbod')); ?>" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Mijn Aanbod</a>
+                            <a href="<?php echo esc_url(home_url('/mijn-account-2')); ?>" class="block px-4 py-2 text-blue-800 hover:bg-gray-200">Mijn Account</a>
+                            <a href="<?php echo wp_logout_url(home_url()); ?>" class="block px-4 py-2 text-red-600 hover:bg-gray-200">Afmelden</a>
+                        </div>
+                    </div>
+                    <script>
+                        document.getElementById('user-menu-button').addEventListener('click', function() {
+                            var menu = document.getElementById('user-menu');
+                            menu.classList.toggle('hidden');
+                        });
+                    </script>
+                <?php endif; ?>
+            </div>
+            <button class="md:hidden text-orange-600">
+                ☰
+            </button>
         </div>
     </header>
-
-    <div class="depth-frame">
-      <div class="group">
-        <div class="overlap-group">
-          <img class="vector" src="img/image.svg" />
-          <img class="img" src="img/vector.svg" />
-          <img class="bildschirmfoto" src="img/bildschirmfoto-2025-06-17-um-23-10-42-removebg-preview-1.png" />
-        </div>
-      </div>
-      <div class="div">
-        <div class="div-2">
-          <div class="div-wrapper"><div class="text-wrapper">Men</div></div>
-          <div class="div-wrapper"><div class="text-wrapper">Women</div></div>
-          <div class="div-wrapper"><div class="text-wrapper">Kids</div></div>
-          <div class="div-wrapper"><div class="text-wrapper">Sale</div></div>
-          <div class="div-wrapper"><div class="text-wrapper">Contact</div></div>
-        </div>
-        <div class="div-3">
-          <div class="depth-frame-wrapper">
-            <div class="depth-frame-wrapper-2">
-              <div class="vector-wrapper"><img class="vector-2" src="img/vector-0.svg" /></div>
-            </div>
-          </div>
-          <div class="depth-frame-wrapper">
-            <div class="depth-frame-wrapper-2">
-              <div class="vector-wrapper"><img class="vector-2" src="img/vector-0-3.svg" /></div>
-            </div>
-          </div>
-          <div class="depth-frame-wrapper">
-            <div class="depth-frame-wrapper-2">
-              <div class="vector-wrapper"><img class="vector-3" src="img/vector-0-2.svg" /></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-</div>
-
-<?php wp_footer(); ?>
 </body>
 </html>
