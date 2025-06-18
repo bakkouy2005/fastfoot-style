f152698cbf42b3260cb82e621615bf011e71940f
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 10:37:23 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,2 +1,10 @@
 <?php 
 
+// Theme setup function
+function fastfoot_style_setup() {
+    add_theme_support('title-tag'); 
+    add_theme_support('post-thumbnails'); 
+    add_theme_support('block-templates'); 
+}
+add_action('after_setup_theme', 'fastfoot_style_setup');
+
