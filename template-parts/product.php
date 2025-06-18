5d6269e8339e8f4af905b5fb8471f17239f2df75
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-19 00:17:10 +0200

    Auto-commit: 2025-06-19 00:17:10

--- template-parts/product.php
+++ template-parts/product.php
@@ -17,7 +17,9 @@ $args = [
 $query = new WP_Query($args);
 ?>
 
-<h2 class="text-4xl font-bold mb-10"><?php echo esc_html($title); ?></h2>
+<div class="px-4 mb-8">
+  <h2 class="text-4xl font-bold"><?php echo esc_html($title); ?></h2>
+</div>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
   <?php while ($query->have_posts()) : $query->the_post(); global $product; ?>
