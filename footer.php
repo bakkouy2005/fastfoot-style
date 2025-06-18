4f6e29b270cef111e8e980f79f2cc01c17059fb8
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 11:01:22 +0200

    Auto-commit: 2025-06-18 11:01:21

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <footer id="colophon" class="site-footer">
+        <div class="container">
+            <div class="site-info">
+                &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. 
+                Alle rechten voorbehouden.
+            </div>
+        </div>
+    </footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
