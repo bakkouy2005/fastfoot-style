6ddc6910e91bbcb031b850df641f1c0bb653466e
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 21:13:30 +0200

    Auto-commit: 2025-06-18 21:13:30

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
             ));
             ?>
             <div class="flex items-center space-x-6">
-                <button class="text-white bg-[#333d33] hover:text-gray-300">
+                <button class="text-white bg-[#333d33] hover:text-gray-300 rounded-full p-2ßßßßß">
                     <i class="fas fa-search text-xl"></i>
                 </button>
                 <button class="text-white hover:text-gray-300">
