020bdcfa6fa3affbf1ef79ed286d414bbfb33dee
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 12:11:23 +0200

    Auto-commit: 2025-06-18 12:11:23

--- function.php
+++ function.php
@@ -9,6 +9,7 @@ function fastfoot_style_setup() {
     add_theme_support('title-tag'); 
     add_theme_support('post-thumbnails'); 
     add_theme_support('block-templates'); 
+    add_theme_support('menus');
     
     // Register navigation menus
     register_nav_menus(
