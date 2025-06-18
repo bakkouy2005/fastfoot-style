52a5ff07b63fbefe8f05544e5552e00873da9405
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 21:07:40 +0200

    Auto-commit: 2025-06-18 21:07:40

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
     <div class="container mx-auto flex justify-between items-center py-4 px-6">
         <!-- Logo -->
         <a href="<?php echo esc_url(home_url('/')); ?>" class="flex items-center">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/image copy 2.png" alt="Fast Foot Style" class="h-22 md:h-20 w-auto md:h-full md:w-48">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image copy 2.png" alt="Fast Foot Style" class="h-22 md:h-20 w-auto md:h-full ">
         </a>
         
         <!-- Main Navigation -->
