e8900e583993f6386a2a2f6f46a34ee658242ada
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 15:55:50 +0200

    Auto-commit: 2025-06-18 15:55:50

--- homepage.php
+++ homepage.php
@@ -4,7 +4,7 @@ get_header();
 ?>
 
 <div class="bg-[#181d18]">
-    <div class="container mx-auto px-4">
+    <div class="container mx-4 px-4">
         <?php get_template_part('template-parts/hero-part'); ?>
     </div>
 </div>
