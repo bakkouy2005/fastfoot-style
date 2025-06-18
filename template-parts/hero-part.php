8af3c7e0063eca246b5a09acab1e8532795effc3
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 16:11:01 +0200

    Auto-commit: 2025-06-18 16:11:01

--- template-parts/hero-part.php
+++ template-parts/hero-part.php
@@ -10,7 +10,7 @@ $search_group = !empty($hero_group['search_group']) ? $hero_group['search_group'
 >
   
   <!-- Logo at the top -->
-  <div class="relative z-10  mx-auto px-8 pt-4">
+  <div class="relative z-10  mx-auto ">
     <img src="<?php echo get_template_directory_uri(); ?>/images/image copy.png" alt="Fast Foot Style" class="h-32 md:h-40 w-auto">
   </div>
 
