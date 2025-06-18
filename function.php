dd5f0cedd6cfd4e9ec1c3ea0ab283417d4483ba2
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-18 12:20:48 +0200

    Auto-commit: 2025-06-18 12:20:48

--- function.php
+++ function.php
@@ -4,7 +4,18 @@
  * 
  */
 
+ function fastfoot_style_enqueue_assets() {
+    // Material Design Bootstrap
+    wp_enqueue_style('mdbootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.2.0/mdb.min.css', array(), '8.2.0', 'all');
 
+    // Custom styles
+    wp_enqueue_style('unique-style', get_template_directory_uri() . '/css/unique.style.css', array(), '1.0', 'all');
+    wp_enqueue_style('unique-globals', get_template_directory_uri() . '/css/unique.globals.css', array(), '1.0', 'all');
+    wp_enqueue_style('unique-animations', get_template_directory_uri() . '/css/unique.animations.css', array(), '1.0', 'all');
+
+    // Custom scripts
+    wp_enqueue_script('unique-script', get_template_directory_uri() . '/js/unique.script.js', array('jquery'), '1.0', true);
+}
 
 // Add custom classes to menu items
 
