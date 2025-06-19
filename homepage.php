f4c51f48b7073e8f6610d5ef4d250534b07b4855
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-19 09:11:47 +0200

    Auto-commit: 2025-06-19 09:11:47

--- homepage.php
+++ homepage.php
@@ -6,7 +6,11 @@ get_header();
 <div class="">
     <div class="container mx-auto ">
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 1]); ?>
-    <?php get_template_part('template-parts/product'); ?>
+    <?php get_template_part('template-parts/product-listing', null, [
+  'term' => 'nieuwste-collectie',
+  'title' => 'Nieuwste collectie',
+  'limit' => 3
+]); ?>
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 2]); ?>
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 3]); ?>
 
