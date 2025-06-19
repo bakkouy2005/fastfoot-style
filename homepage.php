ee30901b5901498678c0914edc5868fd0a00e0ce
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-19 09:14:41 +0200

    Auto-commit: 2025-06-19 09:14:41

--- homepage.php
+++ homepage.php
@@ -7,8 +7,9 @@ get_header();
     <div class="container mx-auto ">
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 1]); ?>
     <?php get_template_part('template-parts/product-listing', null, [
-  'term' => 'New collection',
-  'title' => 'New collection',
+  'term' => 'new-collection', // <-- dit is de slug van de categorie
+  'taxonomy' => 'product_cat',
+  'title' => 'Nieuwste Collectie',
   'limit' => 3
 ]); ?>
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 2]); ?>
