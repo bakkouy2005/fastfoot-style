5d02347657ce7ccd746abbf30394cc90264a67c7
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 12:11:26 +0200

    Auto-commit: 2025-06-24 12:11:26

---
+++ template-parts/slider-leauge.php
@@ -0,0 +1,14 @@
+<div class="px-8 py-5">
+    <div class="swiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/1.svg" alt="1" class="w-full h-full object-cover">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/2.svg" alt="2" class="w-full h-full object-cover">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/3.svg" alt="3" class="w-full h-full object-cover">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/4.svg" alt="4" class="w-full h-full object-cover">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/5.svg" alt="5" class="w-full h-full object-cover">
+                
+            </div>
+        </div>
+    </div>
+</div>
