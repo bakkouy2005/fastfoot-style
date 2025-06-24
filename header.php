c56c2e385953450c4bd6e1a1b8136e3e8a46d537
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 11:47:19 +0200

    Auto-commit: 2025-06-24 11:47:19

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
     <?php wp_head(); ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
