a3d94f3c329ad85d15907570d5f075543ff44636
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 11:15:21 +0200

    Auto-commit: 2025-06-24 11:15:21

--- template-parts/product.php
+++ template-parts/product.php
@@ -32,7 +32,7 @@ $query = new WP_Query($args);
         $image_title = strtolower(get_the_title($image_id));
         if (strpos($image_title, 'achterkant') !== false) {
           $back_image = wp_get_attachment_image($image_id, 'full', false, [
-            'class' => 'w-full h-[490px] object-contain rounded-[12px] absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-300 transform-gpu will-change-transform'
+            'class' => 'w-full h-[490px] object-contain absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-300 transform-gpu will-change-transform'
           ]);
           break;
         }
@@ -40,10 +40,10 @@ $query = new WP_Query($args);
     ?>
       <div class="group relative">
         <div class="relative w-full h-[461px] overflow-hidden bg-[url('/wp-content/themes/fastfoot-style/assets/images/mesh-pattern.png')] bg-cover rounded-[12px]">
-          <a href="<?php the_permalink(); ?>" class="block w-full h-full rounded-[12px] overflow-hidden relative">
+          <a href="<?php the_permalink(); ?>" class="block w-full h-full relative">
             <?php 
               echo $product->get_image('full', [
-                'class' => 'w-full h-[490px] object-contain rounded-[12px] transition duration-300 group-hover:opacity-0 transform-gpu will-change-transform'
+                'class' => 'w-full h-[490px] object-contain transition duration-300 group-hover:opacity-0 transform-gpu will-change-transform'
               ]);
               if ($back_image) echo $back_image;
             ?>
