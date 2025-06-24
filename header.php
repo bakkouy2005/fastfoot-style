9792f7e2281aa4a35e67464dd95969495e1d8ab1
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 12:15:18 +0200

    Auto-commit: 2025-06-24 12:15:18

--- header.php
+++ header.php
@@ -7,10 +7,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <?php wp_head(); ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </head>
 <body <?php body_class('font-inter'); ?>>
 <?php wp_body_open(); ?>
