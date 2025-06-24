36e80220ea222cf7f59ae6c42268001d75d7d8fc
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 10:52:12 +0200

    Auto-commit: 2025-06-24 10:52:12

--- template-parts/product.php
+++ template-parts/product.php
@@ -32,6 +32,7 @@ $query = new WP_Query($args);
       if (strpos($image_title, 'achterkant') !== false) {
         $back_image = wp_get_attachment_image($image_id, 'full', false, [
           'class' => 'w-full h-[490px] object-contain rounded-[12px] absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-300 transform-gpu will-change-transform'
+          
         ]);
         break;
       }
