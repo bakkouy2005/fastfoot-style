e1df800185b09bc5492d47bfd7b74d02df916dbe
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 12:21:17 +0200

    Auto-commit: 2025-06-24 12:21:17

--- template-parts/slider-leauge.php
+++ template-parts/slider-leauge.php
@@ -56,11 +56,11 @@ document.addEventListener('DOMContentLoaded', function() {
         grabCursor: true,
         breakpoints: {
             320: {
-                slidesPerView: 4,
+                slidesPerView: 2,
                 spaceBetween: 20
             },
             640: {
-                slidesPerView: 4,
+                slidesPerView: 3,
                 spaceBetween: 30
             },
             1024: {
