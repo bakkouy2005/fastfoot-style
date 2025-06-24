5e2e37d3648dff59c0c864140e520d24bac55db5
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 11:55:16 +0200

    Auto-commit: 2025-06-24 11:55:16

--- template-parts/product.php
+++ template-parts/product.php
@@ -51,7 +51,7 @@ $query = new WP_Query($args);
         </div>
         <div class="mt-4">
           <h3 class="text-xl font-bold text-[#0A0A0A] font-impact"><?php the_title(); ?></h3>
-          <p class="text-xl text-[#45BD45] font-inter"><?php echo $product->get_price(); ?></p>
+          <p class="text-xl text-[#45BD45] font-inter">€<?php echo $product->get_price(); ?></p>
         </div>
       </div>
     <?php 
