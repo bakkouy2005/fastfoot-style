44d4c1f7276844b32112ad7d1bcef5087f4a78f6
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 11:09:36 +0200

    Auto-commit: 2025-06-24 11:09:36

--- header.php
+++ header.php
@@ -90,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 </script>
 
-<header class="site-header w-full bg-black/5 backdrop-blur-md">
+<header class="site-header w-full bg-black/5 backdrop-blur-md relative z-50">
     <div class="w-full flex justify-between items-center py-4 px-8">
         <!-- Logo -->
         <a href="<?php echo esc_url(home_url('/')); ?>" class="flex items-center relative z-10">
