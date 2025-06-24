427b2b11d8729adda4d26b31544cda3afb19c85b
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 12:15:30 +0200

    Auto-commit: 2025-06-24 12:15:30

--- template-parts/slider-leauge.php
+++ template-parts/slider-leauge.php
@@ -1,14 +1,54 @@
-<div class="px-8 py-5">
-    <div class="swiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/1.svg" alt="1" class="w-full h-full object-cover">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/2.svg" alt="2" class="w-full h-full object-cover">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/3.svg" alt="3" class="w-full h-full object-cover">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/4.svg" alt="4" class="w-full h-full object-cover">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/5.svg" alt="5" class="w-full h-full object-cover">
-                
+<?php
+$logos = [
+    ['src' => 'premier-league.svg', 'alt' => 'Premier League'],
+    ['src' => 'bundesliga.svg', 'alt' => 'Bundesliga'],
+    ['src' => 'laliga.svg', 'alt' => 'La Liga'],
+    ['src' => 'serie-a.svg', 'alt' => 'Serie A'],
+    ['src' => 'ligue-1.svg', 'alt' => 'Ligue 1']
+];
+?>
+
+<div class="bg-black">
+    <div class="swiper league-slider">
+        <div class="swiper-wrapper py-4">
+            <?php foreach ($logos as $logo): ?>
+            <div class="swiper-slide flex items-center justify-center">
+                <img 
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/<?php echo $logo['src']; ?>" 
+                    alt="<?php echo $logo['alt']; ?>" 
+                    class="h-12 w-auto opacity-70 hover:opacity-100 transition-opacity"
+                >
             </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.league-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 40
+            }
+        }
+    });
+});
+</script>
