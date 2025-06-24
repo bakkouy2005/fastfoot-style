46a760249409084492ff7e4e6744a196c640846d
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 12:04:18 +0200

    Auto-commit: 2025-06-24 12:04:18

--- homepage.php
+++ homepage.php
@@ -6,7 +6,6 @@ get_header();
 <div>
     <div>
     <?php get_template_part('template-parts/hero-conf', null, ['hero_index' => 1]); ?>
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/1.svg" alt="1" class="absolute top-0 left-0">
     <?php get_template_part('template-parts/product', null, [
   'term' => 'new-collection', // <-- dit is de slug van de categorie
   'taxonomy' => 'product_cat',
