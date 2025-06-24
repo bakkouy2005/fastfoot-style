e8c5e22a88d4f8a3bd3a663dc14f6be8b87db80f
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2025-06-24 11:37:32 +0200

    Auto-commit: 2025-06-24 11:37:32

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
- 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'impact': ['Impact', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
